b9da300924ccfb9162edc3aee6d224e2f5a2db8f
Shawn Mealey <mealeyst@gmail.com>
2022-03-25 17:32:13 -0400

    Updating the navigation component

--- src/Navigation/index.tsx
+++ src/Navigation/index.tsx
@@ -1,2 +1,9 @@
-import react from 'react';
 import styled from 'styled-components';
+import { color } from '../theme/color';
+import { spacing } from '../theme/spacing';
+
+export const Navigation = styled.nav`
+  display: flex;
+  color: ${color('secondary.blue1')};
+  height: ${spacing(6)}
+`
