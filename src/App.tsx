50bad4fa72d65e017619a763de2530fef149d102
mealeyst <mealeyst@gmail.com>
2022-05-05 23:44:19 -0400

    Adding Segment Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import { Page } from './components/Entries/Page/Page';
 import { Navigation } from './components/Navigation';
 import { BlueTheme } from './theme/Theme';
 
-const App: React.FC = (props) =>
-  <BlueTheme>
-    <>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Page />} />
-        <Route path="/:slug" element={<Page />} />
-      </Routes>
-    </>
-  </BlueTheme>;
+const App: React.FC = (props) => {
+  return (
+    <BlueTheme>
+      <>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Page />} />
+          <Route path="/:slug" element={<Page />} />
+        </Routes>
+      </>
+    </BlueTheme>
+  );
+}
 
 export default App;
