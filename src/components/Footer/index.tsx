03f93a630cbbfa711953e25ec77a8352ecfca24b
mealeyst <mealeyst@gmail.com>
2022-05-13 00:13:10 -0400

    Adding background styling to the footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,6 +5,7 @@ import { up } from '../../theme/mediaQueies';
 import { spacing } from '../../theme/spacing';
 import { Button, Input, Label } from '../Forms';
 import { PawlingDemIconWithText } from '../Icons/PawlingDemIconWithText';
+import { FooterBackground } from './FooterBackground';
 
 export const Footer = styled(({className}) =>
   <footer className={className}>
@@ -24,10 +25,12 @@ export const Footer = styled(({className}) =>
     </section>
   </footer>
 )`
-  background-color: ${color("primary.400")};
+background-color: #0a1f43;
+background-image: linear-gradient(145deg, hsla(219, 59%, 24%, 0.8), hsla(218, 74%, 15%, 0.8)), url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cg fill='%23233f71' fill-opacity='0.4'%3E%3Cpolygon fill-rule='evenodd' points='8 4 12 6 8 8 6 12 4 8 0 6 4 4 6 0 8 4'/%3E%3C/g%3E%3C/svg%3E");
   width: 100%;
   min-height: ${spacing(30)};
   padding: ${spacing(10, 4)};
+  position: relative;
   section {
     max-width: 1440px;
     display: grid;
