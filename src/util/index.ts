936eb097826583c248aed0f52fc55a37453330f5
mealeyst <mealeyst@gmail.com>
2022-06-14 00:21:30 -0400

    Updating files

--- src/util/index.ts
+++
@@ -1,7 +0,0 @@
-export type Obj = Record<string | number | symbol, unknown>;
-
-export const isObj = (raw: unknown): raw is Obj =>
-  typeof raw === 'object'
-    && raw !== undefined
-    && raw !== null
-    && !Array.isArray(raw);
