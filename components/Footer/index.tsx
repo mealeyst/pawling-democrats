77e258ceaa41a82ecb7bdf81d88a641b71468d18
mealeyst <mealeyst@gmail.com>
2022-06-21 23:07:42 -0400

    Adding helpful links section

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -12,6 +12,104 @@ export const Footer = styled(({ className }) => (
   <footer className={className}>
     <div className="footer-content">
       <PawlingDemIconWithText className="logo" />
+      <div className="helpful-links">
+        <h4>Helpful Links:</h4>
+        <nav>
+          <ul>
+            <li>
+              <a
+                href="https://www.pawling.org/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Town of Pawling
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.pawlingschools.org/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Pawling Central School District
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.pawlingfreelibrary.org/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Pawling Free Library
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.ny.gov/services/register-vote"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Register To Vote
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.senate.gov/senators/senators-contact.htm?State=NY"
+                target="_blank"
+                rel="noreferrer"
+              >
+                United States Senators
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.dutchessdems.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Dutchess County Democrats
+              </a>
+            </li>
+            <li>
+              <a href="https://democrats.org/" target="_blank" rel="noreferrer">
+                National Democratic Party
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://elections.dutchessny.gov/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Dutchess County Board of Elections
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://elections.dutchessny.gov/candidate-information/district-maps/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                District Maps
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.thepawlingnews.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                The Pawling Daily Chronicle
+              </a>
+            </li>
+            <li>
+              <a href="https://ag.ny.gov/" target="_blank" rel="noreferrer">
+                Attourney General Letitia James
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
       <div className="social-region">
         <span>Follow us:</span>
         <a href="https://www.facebook.com/pawlingdems/">
@@ -47,6 +145,15 @@ export const Footer = styled(({ className }) => (
     .contact {
       margin: ${spacing(10, 0)};
     }
+    .helpful-links ul{
+      list-style: none;
+      padding-left: 0;
+      display: flex;
+      flex-direction: column;
+      li {
+        padding: ${spacing(1, 0)};
+      }
+    }
     ${query('md')} {
       grid-template-columns: repeat(8, 1fr);
       gap: ${spacing(6)};
@@ -54,14 +161,28 @@ export const Footer = styled(({ className }) => (
         grid-column-start: 1;
         grid-column-end: 5;
       }
+      .helpful-links {
+        grid-column-start: 5;
+        grid-column-end 10;
+        h4 {
+          margin-top: 0;
+        }
+        ul {
+          flex-direction: row;
+          flex-wrap: wrap;
+        }
+        li {
+          width: 50%;
+        }
+      }
       .contact {
         grid-column-start: 5;
         grid-column-end: 9;
         margin: ${spacing(0)};
       }
       .social-region {
-        grid-column-start: 3;
-        grid-column-end: 7;
+        grid-column-start: 2;
+        grid-column-end: 8;
         justify-content: center;
       }
       .copyright-info {
