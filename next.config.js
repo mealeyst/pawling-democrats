23c0c0ed93d9959e7014bb95051b0eca820f913f
mealeyst <mealeyst@gmail.com>
2023-07-05 09:42:07 -0400

    Adding the updated config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.(graphql|gql)/,
+      exclude: /node_modules/,
+      loader: "graphql-tag/loader"
+    });
+
+    return config;
+  }
 }
 
 module.exports = nextConfig
